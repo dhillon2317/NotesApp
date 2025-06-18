a816a1d578b222c289a546cdd83f96c2f1e10951
dhillon <dhillonnirmal578@gmail.com>
2025-06-18 14:42:55 +0530

    updated 3 time

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import store from './Store.js'
+import Store from './Store.js'
 import { ToastContainer } from 'react-toastify';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
+    <Provider store={Store}>
       <App />
       <ToastContainer />
     </Provider>
